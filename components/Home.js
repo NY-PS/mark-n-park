a2c5ef53a0c1d41650359339bc736de9a3c2c646
Angela Kwon <kwon.angela@gmail.com>
2019-03-04 20:03:09 -0500

    create base components

---
+++ components/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#fff"
+        }}
+      >
+        <Text>Home!</Text>
+      </View>
+    );
+  }
+}
+
+export default Home;
